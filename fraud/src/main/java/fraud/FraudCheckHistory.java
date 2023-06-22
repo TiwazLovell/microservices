2415b1dcc7e32f3b512c08152c7f8bfbdcfce90e
Tiwaz Lovell <leonardo_martinas@yahoo.com>
2023-06-22 17:26:32 +0300

    created fraud microservice

--- fraud/src/main/java/fraud/FraudCheckHistory.java
+++ fraud/src/main/java/fraud/FraudCheckHistory.java
@@ -1,4 +1,4 @@
-package customer;
+package fraud;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,25 +10,26 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Customer {
+public class FraudCheckHistory {
 
     @Id
     @SequenceGenerator(
-            name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence"
+            generator = "fraud_id_sequence"
     )
     private Integer id;
-    private String firstName;
-    private String lastName;
-    private String email;
+    private Integer customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
 }
