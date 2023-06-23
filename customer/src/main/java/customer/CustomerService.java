621432ab71b1105d70c3f188d349c0a9808fbd16
Tiwaz Lovell <leonardo_martinas@yahoo.com>
2023-06-23 12:01:26 +0300

    notification

--- customer/src/main/java/customer/CustomerService.java
+++ customer/src/main/java/customer/CustomerService.java
@@ -2,6 +2,8 @@ package customer;
 
 import clients.fraud.FraudCheckResponse;
 import clients.fraud.FraudClient;
+import clients.notification.NotificationClient;
+import clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,9 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
 
-    public void registerCustomer(CustomerRegistrationRequest request){
+    public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
@@ -22,8 +25,16 @@ public class CustomerService {
 
         FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
-        if(fraudCheckResponse.isFraudster()){
+        if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
+
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi, %s !", customer.getFirstName())
+                )
+        );
     }
 }
