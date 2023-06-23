9fb50947a35806d0819631a962fc8ce2a7c4328b
Tiwaz Lovell <leonardo_martinas@yahoo.com>
2023-06-23 08:28:52 +0300

    eureka server

--- customer/src/main/java/customer/CustomerController.java
+++ customer/src/main/java/customer/CustomerController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping(" ")
+@RequestMapping("api/v1/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
 
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("new customer registration {} ", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
     }
